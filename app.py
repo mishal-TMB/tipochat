9cbfd0079282bc513a0d28b4f2c4472059d1229d
TheMiklBraun . <misha_ld_17@mail.ru>
2026-02-13 20:11:31 +0300

    fix local server

--- app.py
+++ app.py
@@ -133,4 +133,4 @@ def handle_disconnect():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port, debug=True)
+    socketio.run(app, host='0.0.0.0', port=port, debug=True, allow_unsafe_werkzeug=True)
